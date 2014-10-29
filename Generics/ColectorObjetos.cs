77a51b3c47af9df423f5022cc6cb64b9e80309d4
TubalLara <tubal.lara@tajamar365.com>
2014-10-29 14:24:19 +0100

    primera subida

---
+++ Generics/ColectorObjetos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class ColectorObjetos <Tipo> where Tipo:Libro
+    {
+
+        private Tipo[] datos;//me creo la propiedad
+
+        public ColectorObjetos(int elementos)//la inicializo
+        {
+            datos=new Tipo[elementos];
+        }
+
+        public Tipo this[int i]//es una propiedad con índices de indexamiento para poder trabajar con ella como si fuera un array
+        {
+            get { return datos[i]; }
+            set { datos[i] = value; }
+
+        }
+
+        public void Borrar(Tipo t)
+        {
+            for(int i=0;i<datos.Length;i++)
+            {
+
+
+                if (datos[i]!=null && datos[i].Equals(t))
+                {
+                    datos[i] = default(Tipo);//default es para que me devuelva el Tipo por defecto de una clase
+                }
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //comento hasta el borrar para poner el ejemplo de lista
+           // ColectorObjetos<Libro> co=new ColectorObjetos<Libro>(3);
+            //var l = new Libro() {Titulo = "La Hoguera e las Vanidades"};
+            //co[0] = new Libro() {Titulo = "El Padrino"};
+            //co[0].Titulo = "El Padrino";
+            //co[1] = l;
+            //co.Borrar(co[0]);
+
+
+            var lista = new List<Libro>();//las diferencias de esto con ArrayList, es que ese es para objects y eso es para genéricos
+
+            lista.Add(new Libro(){Titulo = "Lo que sea"});
+            lista.Add(new Libro() { Titulo = "Lo que sea" });
+            lista.Add(new Libro() { Titulo = "Lo que sea" });
+            lista.Add(new Libro() { Titulo = "Lo que sea" });
+
+            Console.WriteLine(lista[2].Titulo);
+
+            lista.RemoveAt(3);
+
+            Console.WriteLine(lista.Count);
+
+            foreach (var l in lista)
+            {
+                Console.WriteLine(l.Titulo);
+            }
+
+        }
+    }
+}
